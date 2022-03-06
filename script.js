09f04af2d713d1e5882ee48e19ff7499b9288f34
KBasey <baseykieera@gmail.com>
2022-03-05 22:53:42 -0500

    Added HTML and JavaScript file. Added ComputerPlay function that gives random rock, paper, scissor value.

---
+++ script.js
@@ -0,0 +1,6 @@
+ function ComputerPlay() {
+    var values = ["rock","paper","scissors"],
+    valueToUse = values[Math.floor(Math.random() * values.length)];
+            
+    console.log(valueToUse);
+}
